fe6240319460a94b8587d1d33b6a90e770eac828
Abhishek <aks9388@gmail.com>
2021-12-31 03:23:56 +0530

    Renaming files

--- src/main/java/com/aks/securefilemanager/Controller/MainController.java
+++ src/main/java/com/aks/securefilemanager/Controller/MainController.java
@@ -8,7 +8,7 @@ import javafx.stage.Stage;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 
-public class HelloController {
+public class MainController {
 
     @FXML
     private HTMLEditor htmlEditor;
